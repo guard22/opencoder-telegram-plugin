909f3fd569707733b9e9ff47cb386c7a680f1b29
Tommertom <Tom.Gruintjes@gmail.com>
2026-01-17 15:28:43 +0100

    Add barrel file for commands folder

---
+++ plugin/src/commands/index.ts
@@ -0,0 +1,7 @@
+export { createAgentsCommandHandler } from "./agents.js";
+export { createDeleteSessionsCommandHandler } from "./deletesessions.js";
+export { createHelpCommandHandler } from "./help.js";
+export { createMessageTextHandler } from "./message-text.command.js";
+export { createNewCommandHandler } from "./new.js";
+export { createSessionsCommandHandler } from "./sessions.js";
+export type { CommandDeps } from "./types.js";
